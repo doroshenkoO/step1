b1017f7657689df95bd7afc8952afba6dfde24f9
doroshenkoO <ASDoroshenko@gmail.com>
2016-04-02 02:36:31 +0300

    add master

---
+++ ConsoleApplication2/ConsoleApplication2/Point3D.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	internal class Point3D
+	{
+		private int x;
+		private int y;
+		private int z;
+
+		public int X
+		{
+			get { return x; }
+			set { x = value; }
+		}
+
+		public int Y
+		{
+			get { return y; }
+			set { y = value; }
+		}
+
+		public int Z
+		{
+			get { return z; }
+			set { z = value; }
+		}
+
+		public Point3D(int x, int y, int z)
+		{
+			this.X = x;
+			this.Y = y;
+			this.Z = z;
+		}
+
+		public override string ToString()
+		{
+			return String.Format("x = {0}, y = {1}, z = {2}.", this.X, this.Y, this.Z);
+		}
+	}
+}
